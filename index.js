0d72f77d74aa0d4d1da17e1e9a88b605df69a8c9
Robbie Belson <rhbelson@gmail.com>
2018-11-16 13:39:48 -0600

    added 3 charts

--- index.js
+++ index.js
@@ -1,22 +1,54 @@
 
 
+/**
+ * @param latLngInDeg array of arrays with latitude and longtitude
+ *   pairs in degrees. e.g. [[latitude1, longtitude1], [latitude2
+ *   [longtitude2] ...]
+ *
+ * @return array with the center latitude longtitude pairs in 
+ *   degrees.
+ */
+
+
 window.onload = function(){
 document.getElementById("block_view").addEventListener("click", displayBlock);
 document.getElementById("activity_view").addEventListener("click", displayActivity);
+document.getElementById("total_view").addEventListener("click", displayTotal);
+
+
+function dataLoaded() {
+	console.log("Data loaded");
+}
+
+
+function displayTotal() {
+    document.getElementById("myChart").style.display="none";
+    document.getElementById("myChart2").style.display="none";
+    document.getElementById("myChart3").style.display="block";
+    myChart3.update();
+}
 
 
 function displayBlock() {
 	document.getElementById("myChart").style.display="none";
 	document.getElementById("myChart2").style.display="block";
+    document.getElementById("myChart3").style.display="none";
 	myChart2.update();
+	const Http = new XMLHttpRequest();
+	const url='http://hinckley.cs.northwestern.edu/~rbi054/dm_info.txt';
+	Http.open("GET", url);
+	Http.send();
+	Http.onreadystatechange=(e)=>{
+	console.log(Http.responseText)
+	}
 }
 
 function displayActivity() {
 	document.getElementById("myChart").style.display="block";
 	document.getElementById("myChart2").style.display="none";
+    document.getElementById("myChart3").style.display="none";
 	myChart.update();
 }
-
 
 
 var ctx = document.getElementById("myChart").getContext('2d');
@@ -101,6 +133,28 @@ var myChart2 = new Chart(ctx2, {
         }
     }
 });
+
+var ctx3 = document.getElementById("myChart3").getContext('2d');
+var myChart3 = new Chart(ctx3, {
+  type: 'line',
+  data: {
+    // labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+    datasets: [{ 
+        data: [100,114,106,106,107,171,173,191,180, 170],
+        label: "NUDM Committee Members",
+        borderColor: "#3e95cd",
+        fill: false
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Number of Committee Members in Tent'
+    }
+  }
+});
+
 
 
 
